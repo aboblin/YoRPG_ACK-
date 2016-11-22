aeeab8c1aa8da856ff19cba60a328ccb1085ed0a
kingkwong <bkwong@stuy.edu>
2016-11-22 14:27:07 -0500

    made Character.java abstract

--- Character.java
+++ Character.java
@@ -3,9 +3,9 @@
 // HW32 -- Ye Olde Role Playing Game, Expanded
 // 2016-11-20
 
-public class Character
+public abstract class Character
 {
-     // instance vars
+    // instance vars
     protected static int counter = 0;
     protected static String name = "";
     protected static int HP;
@@ -30,37 +30,45 @@ public class Character
     {
 	return def;
     }
+
     //decreases HP by HPlost
     public void lowerHP(int HPlost)
     {
 	HP -= HPlost;
     }
+
     //lowers monster HP by int damage
     public int attack(Character c) 
     {
 	int damage = (int)((str * dmg) - c.getDefense());
-	if(damage<0){
+	if ( damage < 0 ){
 	    damage = 0;
 	}
 	c.lowerHP(damage);
 	return damage;
     }
+
+    public abstract void specialize();
+    public abstract void normalize();
+    public abstract String about();
  
     //def is decreased and dmg is increased, counter idea credits given to Ishtiaque Mahdi!
-    public void specialize() 
-    {
-	def -= 30;
-	dmg += 0.5;
-    counter += 1;
-    }
+    // public void specialize() 
+    // {
+    // 	def -= 30;
+    // 	dmg += 0.5;
+    // 	counter += 1;
+    // }
+
     //def and dmg reset
-    public void normalize() 
-    {
-    def += 30*counter;
-    dmg -= 0.5*counter;
-    }
-    public static String about()
-    {
-        return "Name: " + name + "\n HP: " + HP + "\n Strength: " + str + "\n Defense: " + def + "\n Attack Rating: " + dmg;
-    }
+    // public void normalize() 
+    // {
+    // 	def += 30*counter;
+    // 	dmg -= 0.5*counter;
+    // }
+
+    // public static String about()
+    // {
+    //     return "Name: " + name + "\n HP: " + HP + "\n Strength: " + str + "\n Defense: " + def + "\n Attack Rating: " + dmg;
+    // }
 }
